d82ba8d47d671e86ad8be687f57bd25c157f55dd
luizlima26 <luizhenrilima26@gmail.com>
2026-09-25 16:49:16 -0300

    mais funcoes

--- README.md
+++
@@ -1,3 +0,0 @@
-# grupo11-mini-projeto-alien
-Mini projeto de IP turma 2026/2
-Teste
--- grupo11-mini-projeto-alien.c
+++ grupo11-mini-projeto-alien.c
@@ -4,7 +4,6 @@
 
 int calctamanho(const char palavra[]) {
 
-    //char palavra[N];
     int tamanho = 0;
 
     while (palavra[tamanho] != '\0') {
@@ -14,21 +13,24 @@ int calctamanho(const char palavra[]) {
     return tamanho;
 }
 
-void inverter(const char palavra[]) {
+void inverter(char palavra[], char palavrainvertida[]) {
     int tamanho = calctamanho(palavra);
     int i;
     for(i = tamanho - 1; i >= 0; i--) {
-        printf("%c", palavra[i]);
+        palavrainvertida[i] = palavra[tamanho - 1 - i];
     }
-    printf("\n");
-}
+    palavrainvertida[tamanho] = '\0';}
 
 int main(){
     char palavra[N];
+    char palavrainvertida[N];
+
     scanf("%[^\n]%*c", palavra);
     
     int tamanho = calctamanho(palavra); //funcao de calcular tamanho da palavra
-    inverter(palavra); //funcao de inverter a palavra
+    inverter(palavra, palavrainvertida); //funcao de inverter a palavra
+
+    printf("%s", palavrainvertida); //imprime a palavra invertida
 
     return 0;
 }
